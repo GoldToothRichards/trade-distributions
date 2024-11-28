e3c71ada473124637b95109fd482712c4b9a516d
GoldToothRichards <crabtr26@proton.me>
2024-11-28 08:57:11 -0500

    refactor: Juno -> Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import juno from "@junobuild/vite-plugin";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://trade-distributions.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
-  vite: {
-    plugins: [juno()],
-  },
   devToolbar: {
     enabled: false,
   },
--- juno.config.ts
+++
@@ -1,8 +0,0 @@
-import {defineConfig} from '@junobuild/config';
-
-export default defineConfig({
-  satellite: {
-    id: 'qox35-uiaaa-aaaal-arskq-cai',
-    source: 'dist'
-  }
-});
